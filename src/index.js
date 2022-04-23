7e4856a669e1b98c1d19de3d7b4a645fa4a7e053
michael bazhanov <michaelbazhanov33@gmail.com>
2022-04-23 14:26:27 +0300

    fix sidebar end

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import './styles/index.pcss'
 
-// import $ from 'jquery' //Если она не нужна то закоммить====
-// =========================================================
-
-// const userStack = {
-// 	language: 'JavaScript',
-// 	framework: 'Angular',
-// }
-// const user = {
-// 	name: 'Michael',
-// 	age: '30',
-// 	...userStack
-// }
-
-// console.log(user)
-// =========================================================
-// $('.jquery').html('jquery work!')
 
 // ### #toggle-menu
 window.onload = function () {
@@ -23,26 +7,17 @@ window.onload = function () {
   let navigation = document.querySelector('#navigation')
   let main = document.querySelector('#main')
   menu.addEventListener('click', (e) => {
-    // console.log(e.eventPhase + 'menu')
-    // if (e.eventPhase === 2) {
-    // console.log('menu')
-    // console.log(e.target)
-    // }
 		menu.classList.toggle('active')
 		navigation.classList.toggle('active')
 		main.classList.toggle('active')
   })
-  // menu.click() //test
 
-  //   let svg = document.querySelector('#svg')
-  //   svg.addEventListener(
-  //     'click',
-  //     (e) => {
-  //       console.log(e.eventPhase + 'svg')
-  //       if (e.eventPhase === 2) {
-  //         console.log('svg')
-  //         console.log(e.target)
-  //       }
-  //     }
-  //   )
+  // Программный hover
+  let list = document.querySelectorAll('#navigation li .navigation-item')
+  console.log(list)
+  list.forEach(item => item.addEventListener('mouseover', activeLink))
+  function activeLink() {
+    list.forEach(item => item.classList.remove('hovered'))
+    this.classList.add('hovered')
+  }
 }
